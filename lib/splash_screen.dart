b7d3418340072e374f9cfc924a86c81bf6cb7d2e
Lester Corpuz <llCorpuz@mcm.edu.ph>
2021-11-25 19:48:07 +0800

    logo style change

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -20,29 +20,18 @@ class _SplashScreenState extends State<SplashScreen> {
                   begin: Alignment.topRight,
                   end: Alignment.bottomLeft,
                   colors: <Color>[
-                Color(0xff272640),
-                Color(0xff006466),
+                Color(0xff3e3e65),
+                Color(0xff1b3a4b),
               ])),
         ),
         Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Expanded(
-              flex: 2,
-              child: Container(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const <Widget>[
-                  CircleAvatar(
-                      backgroundColor: Colors.white,
-                      radius: 50.0,
-                      child: Icon(
-                        Icons.payment,
-                        color: Colors.green,
-                        size: 30.0,
-                      )),
-                ],
-              )),
-            )
+            Image.asset(
+              'assets/images/splash_icon.png',
+              width: 200,
+            ),
+            Text('')
           ],
         )
       ],
