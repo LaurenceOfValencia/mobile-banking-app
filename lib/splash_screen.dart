08037f519f412beb22d35cd7ebf3e3b73aad13f4
Lester Corpuz <llCorpuz@mcm.edu.ph>
2021-11-25 19:57:47 +0800

    splash screen update
    removed unused code

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -10,31 +10,24 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Stack(
-      fit: StackFit.expand,
-      children: <Widget>[
-        Container(
-          decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                  colors: <Color>[
-                Color(0xff3e3e65),
-                Color(0xff1b3a4b),
-              ])),
-        ),
-        Column(
+    return Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: <Color>[
+              Color(0xff3e3e65),
+              Color(0xff1b3a4b),
+            ])),
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Image.asset(
               'assets/images/splash_icon.png',
               width: 200,
             ),
-            Text('')
+            const Text('placeholder')
           ],
-        )
-      ],
-    ));
+        ));
   }
 }
